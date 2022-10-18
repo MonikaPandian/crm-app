dce389728cfa9e0e0eaf622b7020819218aeacd2
MonikaPandian <panmonikmm@gmail.com>
2022-10-18 15:37:48 +0530

    crm-backend

--- routes/users.js
+++ routes/users.js
@@ -34,83 +34,91 @@ router.post("/forgot-password", async (request, response) => {
         //Make sure user exists in database
         const oldUser = await client.db("b37wd").collection("users").findOne({ email: email })
         if (!oldUser) {
-            response.status(400).send({ message: "User not exists !! Please signup and create a new one" })
+            response.status(400).send({ message: "User not exists!!" })
             return;
         }
-        // const secret = process.env.SECRET_KEY + oldUser.password
-        // const payload = {
-        //     email: oldUser.email,
-        //     id: oldUser._id
-        // }
-        // //User exist and now create a one time link valid for 15 minutes
-        // const token = jwt.sign(payload, secret, { expiresIn: '15m' });
-        // const link = `http://localhost:3000/reset-password/${oldUser._id}/${token}`;
-        // console.log(link)
-        response.send(oldUser)
+        const secret = process.env.SECRET_KEY + oldUser.password
+        const payload = {
+            email: oldUser.email,
+            id: oldUser._id
+        }
+        //User exist and now create a one time link valid for 15 minutes
+        const token = jwt.sign(payload, secret, { expiresIn: '15m' });
+        const link = `http://localhost:3000/reset-password/${oldUser._id}/${token}`;
+        var transporter = NodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'panmonikmm@gmail.com',
+                pass: 'lxkugchepioxgtmr'
+            }
+        });
+        var mailOptions = {
+            from: 'panmonikmm@gmail.com',
+            to: `${oldUser.email}`,
+            subject: 'Password reset link',
+            html: `We have received your request for reset password. Click this link to reset your password.<br>
+                  <a href = ${link}>Click Here</a><br>
+                  <p>This link is valid for 15 minutes from your request initiation for password recovery.</p>`
+        };
+    
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                console.log('Email sent:' + info.response);
+            }
+        })  
+        response.send({message: "Email sent successfully"})
     } 
     catch(error){
         response.send({ status: "error", data: error})
     }
 
-    // var transporter = NodeMailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: 'panmonikmm@gmail.com',
-    //         pass: 'lxkugchepioxgtmr'
-    //     }
-    // });
-
-    // var mailOptions = {
-    //     from: 'panmonikmm@gmail.com',
-    //     to: `${userFromDB.username}`,
-    //     subject: 'Password reset link',
-    //     html: `We have received your request for reset password. Click this link to reset your password.<br>
-    //           <a href = ${link}>Click Here</a><br>
-    //           <p>This link is valid for 15 minutes from your request initiation for password recovery.</p>`
-    // };
-
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     else {
-    //         console.log('Email sent:' + info.response);
-    //     }
-    // })
-
-    // response.send({ message: "success" });
+   
 })
 
 //reset password
-router.post("/reset-password", async (request, response) => {
-    const { id, token, password } = request.body;
-
+router.get("/reset-password/:id/:token", async (request, response) => {
+    const { id, token } = request.params;
     //check if this id exist in database
-    const employeeFromDB = await client.db("b37wd").collection("employees").findOne({ _id: ObjectId(id) })
-
-    if (!employeeFromDB) {
-        response.status(400).send({ message: "Enter a valid and registered email Id" })
+    const oldUser = await client.db("b37wd").collection("users").findOne({ _id: ObjectId(id) })
+    if(!oldUser){
+        response.status(400).send({ message: "User not exists!!"})
         return;
     }
+    const secret = process.env.SECRET_KEY + oldUser.password;  
+    try{
+        const verify = jwt.verify(token,secret)
+        response.send("Verified")
+    }
+    catch(error){
+        response.send("Not Verified")
+    }           
+    }
+)
 
-    const secret = process.env.SECRET_KEY + employeeFromDB.password;
-    try {
-        const payload = jwt.verify(token, secret)
-        
-        if (payload) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password, salt)
-            const updatePassword = await client.db("b37wd").collection("employees").updateOne({ _id: ObjectId(id) }, { $set: {password : hashedPassword} })
-            response.send({ message: "Password successfully reset",password:updatePassword })
-        }
-        else{
-            response.send({message : "Token Expired"})
-        }
+router.post("/reset-password/:id/:token", async (request, response) => {
+    const { id, token } = request.params;
+    const { password } = request.body;
+
+    //check if this id exist in database
+    const oldUser = await client.db("b37wd").collection("users").findOne({ _id: ObjectId(id) })
+    if(!oldUser){
+        response.status(400).send({ message: "User not exists!!"})
+        return;
     }
-    catch (error) {
-        console.log(error.message)
-        response.send(error.message)
+    const secret = process.env.SECRET_KEY + oldUser.password;  
+    try{
+        const verify = jwt.verify(token,secret)
+        const encryptedPassword = await bcrypt.hash(password,10)
+        const updatePassword = await client.db("b37wd").collection("users").updateOne({ _id: ObjectId(id) }, { $set: {password : encryptedPassword} })
+        response.send({message: "Password updated"})
     }
+    catch(error){
+        response.send({message: "Something went wrong"})
+    }           
 })
+
 
 export const usersRouter = router;
